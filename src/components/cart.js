a5fa33ed9fb33f9e7d659627e9e1f5f37d5eb28f
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-03-18 17:26:03 -0400

    shop

--- src/components/cart.js
+++ src/components/cart.js
@@ -6,6 +6,10 @@ import { HiOutlineXMark } from "react-icons/hi2"
 
 const Cart = ({ toggleCart }) => {
   const { cart, checkout } = useStore()
+  const formattedNum = num =>
+    Number(num)
+      .toFixed(2)
+      .replace(/[.,]00$/, "")
 
   return (
     <section className={styles.container}>
@@ -25,15 +29,27 @@ const Cart = ({ toggleCart }) => {
       <article className={styles.cartSummary}>
         <div className={styles.checkoutInfo}>
           <div>SUBTOTAL</div>
-          <div>${checkout.subtotalPrice?.amount || 0}</div>
+          <div>
+            $
+            {checkout.subtotalPrice
+              ? formattedNum(checkout.subtotalPrice?.amount)
+              : 0}
+          </div>
         </div>
         <div className={styles.checkoutInfo}>
           <div>TAX</div>
-          <div>${checkout.totalTax?.amount || 0}</div>
+          <div>
+            ${checkout.totalTax ? formattedNum(checkout.totalTax?.amount) : 0}
+          </div>
         </div>
         <div className={styles.checkoutInfo}>
           <div>TOTAL</div>
-          <div>${checkout.totalPrice?.amount || 0}</div>
+          <div>
+            $
+            {checkout.totalPrice
+              ? formattedNum(checkout.totalPrice?.amount)
+              : 0}
+          </div>
         </div>
         <button
           disabled={cart.length === 0}
