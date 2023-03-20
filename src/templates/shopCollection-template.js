7c9b66c01029278ab14f2bc777766b5f9a40f7c5
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-03-20 10:19:39 -0400

    collections

--- src/templates/shopCollection-template.js
+++ src/templates/shopCollection-template.js
@@ -1,9 +1,39 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
+import * as styles from "../components/collection.module.css"
 import Layout from "../components/layout"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 const ShopCollection = ({ data, location }) => {
-  return <Layout location={location}></Layout>
+  const { products } = data.shopifyCollection
+  return (
+    <Layout location={location}>
+      <section className={styles.productContainer}>
+        {products?.map(product => (
+          <Link to={`/shop/${product.handle}`} className={styles.productCard}>
+            <GatsbyImage
+              image={
+                product.featuredImage.localFile.childImageSharp.gatsbyImageData
+              }
+              alt={product.title}
+              className={styles.image}
+            ></GatsbyImage>
+            <article className={styles.productInfoContainer}>
+              <h2 className={styles.productInfo}>
+                <span>{product.metafield.value}</span>
+              </h2>
+              <h3 className={styles.productInfo}>
+                <span>{product.title}</span>
+              </h3>
+              <h3 className={styles.productInfo}>
+                <span>${product.priceRangeV2.minVariantPrice.amount}</span>
+              </h3>
+            </article>
+          </Link>
+        ))}
+      </section>
+    </Layout>
+  )
 }
 
 export const query = graphql`
