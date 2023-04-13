bc2cd4bc74138321b969881ac650dd2d58d315c7
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-04-13 12:16:59 -0400

    tickets

--- src/pages/tickets.js
+++ src/pages/tickets.js
@@ -1,12 +1,61 @@
 import React, { useEffect, useState } from "react"
 import Loader from "../components/loader"
 import Layout from "../components/layout"
-import TicketTile from "../components/ticketTile"
-import * as styles from '../components/ticketsPage.module.css'
+import * as styles from "../components/ticketsPage.module.css"
 
 const Tickets = ({ location }) => {
-  const [events, setEvents] = useState()
+  const [events, setEvents] = useState(null)
   const [loading, setLoading] = useState(true)
+  const [production, setProduction] = useState(null)
+  const [productionDate, setProductionDate] = useState(null)
+  const [time, setTime] = useState(null)
+
+  let eventOptions
+  let dateOptions
+  let timeOptions
+  let url
+  let hasTix = true
+
+  if (events) {
+    eventOptions = [
+      ...new Map(
+        events.performances.map(performance => [
+          performance.productionId,
+          performance,
+        ])
+      ).values(),
+    ]
+  }
+
+  if (production) {
+    const performanceDates = events.performances.filter(
+      performance => performance.productionId.toString() === production
+    )
+    dateOptions = [
+      ...new Map(
+        performanceDates.map(performance => [
+          performance.performanceDate,
+          performance,
+        ])
+      ).values(),
+    ]
+  }
+
+  if (productionDate) {
+    timeOptions = events.performances
+      .filter(performance => performance.productionId.toString() === production)
+      .filter(performance => performance.performanceDate === productionDate)
+  }
+
+  if (production && productionDate && time) {
+    url = `https://ci.ovationtix.com/35572/production/${production}?performanceId=${time}`
+  }
+
+  if (time) {
+    hasTix = events.performances.filter(
+      performance => performance.performanceId.toString() === time
+    )[0].hasAvailableTickets
+  }
 
   useEffect(() => {
     fetch("https://api.ovationtix.com/public/events/client(35572)")
@@ -20,13 +69,75 @@ const Tickets = ({ location }) => {
   return (
     <Layout location={location} title="Tickets">
       <div className={styles.pageContainer}>
-        {loading ? (
-          <Loader></Loader>
-        ) : (
-          events.performances.map(performance => (
-            <TicketTile performance={performance}></TicketTile>
-          ))
-        )}
+        <div className={styles.ticketsContainer}>
+          {loading ? (
+            <Loader></Loader>
+          ) : (
+            <div className={styles.formContainer}>
+              <select
+                className={styles.dropDown}
+                value={production}
+                onChange={e => setProduction(e.target.value)}
+              >
+                <option value="">Events</option>
+                {eventOptions.map(performance => (
+                  <option
+                    value={performance.productionId}
+                    key={performance.productionId}
+                  >
+                    {performance.productionName}
+                  </option>
+                ))}
+              </select>
+              <select
+                className={styles.dropDown}
+                disabled={!production}
+                value={productionDate}
+                onChange={e => setProductionDate(e.target.value)}
+              >
+                <option value="">Date</option>
+                {dateOptions?.map(performance => (
+                  <option
+                    value={performance.performanceDate}
+                    key={performance.performanceDate}
+                  >
+                    {performance.performanceDate}
+                  </option>
+                ))}
+              </select>
+              <select
+                className={styles.dropDown}
+                disabled={!productionDate}
+                value={time}
+                onChange={e => setTime(e.target.value)}
+              >
+                <option value="">Time</option>
+                {timeOptions?.map(performance => (
+                  <option
+                    value={performance.performanceId}
+                    key={performance.performanceId}
+                  >
+                    {performance.performanceTime}
+                  </option>
+                ))}
+              </select>
+              <a
+                href={url}
+                target="_blank"
+                rel="noreferrer"
+                className={
+                  url === undefined || !hasTix
+                    ? styles.checkoutDisabled
+                    : styles.checkout
+                }
+              >
+                {hasTix
+                  ? "Proceed to Ticket Selection and Checkout"
+                  : "Sold Out"}
+              </a>
+            </div>
+          )}
+        </div>
       </div>
     </Layout>
   )
