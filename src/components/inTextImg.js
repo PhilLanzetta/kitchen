8d4eef3e0fc1273106f337db2d79c76690d5d7b9
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-04-26 12:10:22 -0400

    in text img

--- src/components/inTextImg.js
+++ src/components/inTextImg.js
@@ -5,7 +5,7 @@ import * as styles from './inTextImg.module.css'
 const InTextImg = ({data}) => {
   return (
     <figure className={styles.imgContainer}>
-     <GatsbyImage image={data.image.gatsbyImageData} alt={data.image.description}></GatsbyImage>
+     <GatsbyImage image={data.image?.gatsbyImageData} alt={data.image?.description}></GatsbyImage>
      <figcaption>{data.creditText}</figcaption>
      </figure>
   )
