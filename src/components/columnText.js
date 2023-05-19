616347428c8e303bf1274223b40c2d8a2e854e76
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-05-19 13:00:18 -0400

    two-column text

--- src/components/columnText.js
+++ src/components/columnText.js
@@ -1,14 +1,20 @@
 import React from "react"
 import useWindowSize from "../utils/useWindowSize"
+import { marked } from "marked"
 
 const ColumnText = ({ data }) => {
   const { width } = useWindowSize()
   const styles = {
-    columnCount: width > 700 ? data.numberOfColumns : "2",
+    columnCount: width > 700 ? data.numberOfColumns : "1",
     padding: "20px",
     columnGap: "20px",
   }
-  return <div style={styles}>{data.text.text}</div>
+  return (
+    <div
+      style={styles}
+      dangerouslySetInnerHTML={{ __html: marked.parse(data.text.text) }}
+    ></div>
+  )
 }
 
 export default ColumnText
