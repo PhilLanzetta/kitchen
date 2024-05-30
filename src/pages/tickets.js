82398650b41eb5fd1813d3d45257593d9610a1be
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-05-30 14:05:30 -0400

    case test for fetch

--- src/pages/tickets.js
+++ src/pages/tickets.js
@@ -66,7 +66,7 @@ const Tickets = ({ location, data }) => {
 
   const fetchHeaders = JSON.stringify({
     clientId: "35572",
-    Origin: "https://thekitchen.org",
+    origin: "https://thekitchen.org",
   })
 
   useEffect(() => {
